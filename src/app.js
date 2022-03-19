81ee7489de15d079c98bf5c4c396feadbeb65d51
ehabhalil <ehabhalil4@gmail.com>
2022-03-19 03:59:13 +0300

    Feat: add post routes and controllers functions

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ require('dotenv').config();
 const app = express();
 const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
+const indexRouter = require('./routers/index');
+
 const { connectToMongo } = require('./db/connection');
 
 const port = process.env.NODE_LOCAL_PORT || 3000;
 app.use(express.json());
 
+app.use('/', indexRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
+
 app.listen(port, () => {
   console.log('app listening on port 3000!');
   connectToMongo();
--- src/routers/post.js
+++ src/routers/post.js
@@ -4,6 +4,34 @@ const router = express.Router();
 
 const postController = require('../controllers/post');
 
+const isAuthenticated = require('../middlewares/isAuth');
+
 router.get('/', postController.getAllPosts);
+
+router.post('/', isAuthenticated, postController.addNewPost);
+
+router.put('/:id', isAuthenticated, postController.updatePost);
+
+router.delete('/:id', isAuthenticated, postController.deletePost);
+
+router.get('/search', postController.searchForPosts);
+
+router.get('/filter/:tags', postController.getPostsWithSimilarTags);
+
+router.get('/:id', postController.getOnePost);
+
+router.post('/:id/comments', isAuthenticated, postController.addComment);
+
+router.put(
+  '/:id/comments/:commentid',
+  isAuthenticated,
+  postController.updateComment
+);
+
+router.delete(
+  '/:id/comments/:commentid',
+  isAuthenticated,
+  postController.deleteComment
+);
 
 module.exports = router;
