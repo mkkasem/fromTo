80bc07bac5e22dc396de551cccbc37202611111f
mahmoud <mahmoudalkasem1997@gmail.com>
2022-03-23 01:29:44 +0300

    edit isAuth function

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+const cookieParser = require('cookie-parser');
 const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
 const indexRouter = require('./routers/index');
@@ -12,6 +13,7 @@ const logger = require('./services/logger');
 
 const port = process.env.NODE_LOCAL_PORT || 3000;
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/api/auth', authRouter);
